171951f503108188da97eb1d09befe5437043e97
oleg <lambda_core@mail.ru>
2016-06-18 18:30:43 +0800

    Rule 1 for (word) vendor works

---
+++ matter/phone.cxx
@@ -0,0 +1,9 @@
+#encoding "utf8"
+
+PhoneWord -> Word<kwtype="телефон_слово">;
+
+PhoneVendorRus -> Word<kwtype="телефон_производитель_рус">;
+PhoneVendorEng -> Word<kwtype="телефон_производитель_англ">;
+PhoneVendor -> PhoneVendorRus | PhoneVendorEng;
+
+S -> (PhoneWord interp(Phone.Word)) PhoneVendor interp(Phone.Vendor);
