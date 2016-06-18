8413bdd841707a0e18aa0df2b4081ef959f5eb88
oleg <lambda_core@mail.ru>
2016-06-18 18:56:44 +0800

    Should work on model extraction

--- matter/phone.cxx
+++ matter/phone.cxx
@@ -6,5 +6,8 @@ PhoneVendorRus -> Word<kwtype="телефон_производитель_рус"
 PhoneVendorEng -> Word<kwtype="телефон_производитель_англ">;
 PhoneVendor -> PhoneVendorRus | PhoneVendorEng;
 
-S -> PhoneWord interp(Phone.Word) (PhoneVendor interp(Phone.Vendor));
-S -> (PhoneWord interp(Phone.Word)) PhoneVendor interp(Phone.Vendor);
+PhoneModel -> UnknownPOS* UnknownPOS;
+//PhoneModel -> AnyWord<wff=/(([A-Za-z/ -]{0,19}\d)[A-Za-z0-9/ -]{4,20})/>
+
+S -> PhoneWord interp(Phone.Word) (PhoneVendor interp(Phone.Vendor)) (PhoneModel interp(Phone.Model));
+S -> (PhoneWord interp(Phone.Word)) PhoneVendor interp(Phone.Vendor) (PhoneModel interp(Phone.Model));
