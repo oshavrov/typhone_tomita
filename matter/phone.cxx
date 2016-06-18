bbc695b9f29adc32fcfda43c6dee154f6a64365d
oleg <lambda_core@mail.ru>
2016-06-18 18:40:26 +0800

    Forking works with phone matter extraction

--- matter/phone.cxx
+++ matter/phone.cxx
@@ -6,4 +6,5 @@ PhoneVendorRus -> Word<kwtype="телефон_производитель_рус"
 PhoneVendorEng -> Word<kwtype="телефон_производитель_англ">;
 PhoneVendor -> PhoneVendorRus | PhoneVendorEng;
 
+S -> PhoneWord interp(Phone.Word) (PhoneVendor interp(Phone.Vendor));
 S -> (PhoneWord interp(Phone.Word)) PhoneVendor interp(Phone.Vendor);
