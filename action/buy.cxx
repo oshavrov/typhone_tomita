b40798ae5ac254747ce3380183865c48b71ed446
oleg <lambda_core@mail.ru>
2016-06-20 14:47:34 +0800

    Buy by number added to actions

--- action/buy.cxx
+++ action/buy.cxx
@@ -1,3 +1,6 @@
 #encoding "utf-8"
 
-BuyWord -> Word<kwtype="купить_слово"> interp(Buy.Word);
+BuyWord -> Word<kwtype="купить_слово">;
+
+BuyAction -> BuyWord interp(Buy.Word);
+BuyAction -> BuyWord interp(Buy.Word) AnyWord<wff=/\d{1,2}/> interp(Buy.Number);
