4b10bbf4372fa7708f2747f2affc8db843f1f612
oleg <lambda_core@mail.ru>
2016-06-20 14:02:54 +0800

    Word +\d{1,2} removed from matter grammar

--- matter/notebook/notebook.cxx
+++ matter/notebook/notebook.cxx
@@ -10,9 +10,6 @@ NotebookModel -> UnknownPOS* AnyWord<wff=/\w\d/>;
 
 NotebookUserDefinedName -> AnyWord<kwtype="ноутбук_пользовательское_название">;
 
-NotebookNumberInPrice -> AnyWord<wff=/\d{1,2}/>;
-
 S -> NotebookWord interp(Notebook.Word) (NotebookVendor interp(Notebook.Vendor)) (NotebookModel interp(Notebook.Model));
 S -> (NotebookWord interp(Notebook.Word)) NotebookVendor interp(Notebook.Vendor) (NotebookModel interp(Notebook.Model));
 S -> NotebookUserDefinedName interp(Notebook.UserDefinedName);
-S -> NotebookWord interp(Notebook.Word) NotebookNumberInPrice interp(NotebookFromPrice.NumberInPrice);
