3ba6aea04034192245a1bd0a292ceca399ed6638
oleg <lambda_core@mail.ru>
2016-06-26 17:47:43 +0800

    Other matter entity added

--- matter/matter.cxx
+++ matter/matter.cxx
@@ -4,8 +4,10 @@ Phone -> AnyWord<kwtype="телефон">;
 Laptop -> AnyWord<kwtype="ноутбук">;
 Tablet -> AnyWord<kwtype="планшет">;
 TV -> AnyWord<kwtype="телевизор">;
+OtherMatter -> AnyWord<kwtype="другой_предмет">;
 
 S -> Phone interp(Matter.Phone);
 S -> Laptop interp(Matter.Notebook);
 S -> Tablet interp(Matter.Tablet::not_norm);
 S -> TV interp(Matter.TV);
+S -> OtherMatter interp(Matter.Other);
---
+++ matter/other/other.cxx
@@ -0,0 +1,15 @@
+#encoding "utf8"
+
+SpecificMatter -> Adj<gnc-agr[1]> Noun<gnc-agr[1]>;
+OtherWord -> AnyWord<kwtype="другой_предмет_слово">;
+OtherVendorRus -> Word<kwtype="другой_предмет_производитель_рус">;
+OtherVendorEng -> Word<kwtype="другой_предмет_производитель_англ">;
+OtherVendor -> OtherVendorRus | OtherVendorEng;
+OtherUserDefinedName -> AnyWord<kwtype="другой_предмет_пользовательское_название">;
+
+S -> SpecificMatter;
+S -> OtherWord (OtherVendor) (OtherWord);
+S -> (OtherWord) OtherVendor (OtherWord);
+S -> OtherUserDefinedName (OtherVendor) (OtherWord);
+S -> (OtherVendor) OtherUserDefinedName;
+S -> OtherUserDefinedName;
