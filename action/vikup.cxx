de0bf3b0125eee7413e03615dbd812fd87f6633f
oleg <lambda_core@mail.ru>
2016-06-28 16:36:19 +0800

    "Продать" работает только для непрошедшего времени

--- action/vikup.cxx
+++ action/vikup.cxx
@@ -1,6 +1,6 @@
 #encoding "utf-8"
 
-CustomerSellsWord -> Word<kwtype="продать_слово">;
+CustomerSellsWord -> Word<kwtype="продать_слово", gram="inpraes">;
 
 CustomerSellsAction -> CustomerSellsWord interp(CustomerSells.Word);
 CustomerSellsAction -> CustomerSellsWord interp(CustomerSells.Word) AnyWord<wff=/\d{1,2}/> interp(CustomerSells.Matter);
