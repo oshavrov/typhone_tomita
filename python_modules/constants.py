9488f4b1268a7af1eafd00c7633bdb27c3d7554c
oleg <lambda_core@mail.ru>
2016-07-02 11:10:04 +0800

    Pawn join to other fixed

---
+++ python_modules/constants.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+XML_CONFIG_NAME = os.path.normpath(r'config_xml.proto')
+TEXT_INPUT = os.path.normpath(r'IO/manual_input.txt')
+TEXT_OUTPUT = os.path.normpath(r'IO/preprocessed_input.txt')
+
+SOURCE_DATE_TEMPLATE = re.compile(r'(\d{2})(?:/|\.)(\d{2})(?:/|\.)(\d{4}).?')
+INSIDE_DATE_TEMPLATE = re.compile(r'\d{2}/\d{2}/\d{4}')
+
+DATE_TEMPLATE = re.compile(r'\d{2}.\d{2}.\d{4}')
+
+# For classifier
+INPUT_FILE = os.path.normpath(r'IO\output.xml')
+PREPROCESSED_INPUT = os.path.normpath(r'IO\preprocessed_input.txt')
+OUTPUT_EXCEL = os.path.normpath(r'IO\report.xlsx')
+SAME_CATEGORY_DELIMITER = r'__'
+EMPTY_COLS = ["Комплектация", "Цена клиента", "Наша цена", "Утилизация", "Решение клиента"]
--- python_modules/input_preprocessor.py
+++ python_modules/input_preprocessor.py
@@ -1,17 +1,7 @@
-import re
-import subprocess
-import sys
-import classifier
-import time
 import os.path
 import shutil
-
-SOURCE_DATE_TEMPLATE = re.compile(r'(\d{2})\.(\d{2})\.(\d{4}).?')
-INSIDE_DATE_TEMPLATE = re.compile(r'\d{2}/\d{2}/\d{4}')
-
-XML_CONFIG_NAME = "config_xml.proto"
-TEXT_INPUT = os.path.normpath(r"IO/manual_input.txt")
-TEXT_OUTPUT = os.path.normpath(r'IO/preprocessed_input.txt')
+import re
+from python_modules.constants import *
 
 def move_txt_config_to_xml_one():
     config_txt = open("config_txt.proto", 'r', encoding="utf8")
@@ -32,15 +22,15 @@ def threat_dates_in_input(input_filename=TEXT_INPUT, output_filename=TEXT_OUTPUT
     preprocessed_input = open(output_filename, "w", encoding="utf8")
 
     date_string = ""
-    for line in f.read().splitlines():
+    file_iterator = f.read().splitlines()
+
+    for line in file_iterator:
         if re.match(SOURCE_DATE_TEMPLATE, line) is not None:
             date_string = re.sub(SOURCE_DATE_TEMPLATE, r'\1/\2/\3', line);
         else:
-            if line:
+            if re.match(r'.{3}', line):
                 prepared_string = date_string + " " + line
                 preprocessed_input.write(prepared_string + "\n")
-
-            
     f.close()
     preprocessed_input.close()
 
@@ -59,32 +49,15 @@ def replace_dots_in_line(input_filename=TEXT_OUTPUT):
             without_dots = re.sub(r'\.(.)', r' \1', without_dots)
             # make date with dots back
             without_dots = re.sub(r'(\d{2})/(\d{2})/(\d{4})', r'\1.\2.\3', without_dots)
-            print(without_dots)
             preprocessed_input.write(without_dots + ".\n")
     f.close()
     preprocessed_input.close()
     shutil.move(output_filename, input_filename);
 
 def preprocess_input():
+    move_txt_config_to_xml_one()
     threat_dates_in_input()
     replace_dots_in_line()
-    
-
-def run_tomita():
-    subprocess.call(["tomitaparser.exe", XML_CONFIG_NAME])
-
-
-def invoke():
-    start = time.time()
-    move_txt_config_to_xml_one()
-    preprocess_input()
-    run_tomita()
-    classifier.classify()
-    end = time.time()
-    print("Затраченное время:", end - start, "сек.")
-
+ 
 if __name__ == "__main__":
-    invoke()
-    print()
-    input("Нажмите Enter для выхода")
-    
+    preprocess_input() 
